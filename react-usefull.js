ea685cca7722c57ca42cecd9838aa940f7a64855
Jyotiraditya Singh <77264025+jyotir07@users.noreply.github.com>
2024-01-10 19:55:37 +0530

    Added an object

--- react-usefull.js
+++ react-usefull.js
@@ -2,3 +2,10 @@
 let age = 17;
 let name = age > 18 ? "Adult" : "Not an adult";
 console.log(name);
+
+//defining an object in js
+const person = {
+  name: "My Name",
+  age: 18,
+  isMarried: false,
+};
