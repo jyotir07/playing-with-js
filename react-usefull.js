bdfc1d19d61621eac32fdad118327714c37f1243
Jyotiraditya Singh <77264025+jyotir07@users.noreply.github.com>
2024-01-10 20:28:05 +0530

    Example arrays modification

--- react-usefull.js
+++ react-usefull.js
@@ -16,3 +16,10 @@ const person2 = {...person, name: "Another Name"};
 //using the defined names
 console.log(person)
 console.log(person2)
+
+//we can use the same thing for arrays
+const arr1 = ["Name1", "Name2", "Name3"];
+const arr2 = [...arr1, "Name4", "Name5"];
+console.log(arr1)
+
+console.log(arr2)
